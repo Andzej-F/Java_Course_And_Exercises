b83136e7eea39f73fb68699cef9f4da966e70d13
Andzej-F <70884391+Andzej-F@users.noreply.github.com>
2023-06-26 19:18:43 +0300

    it's been a while since last commit :), let's see if it works

--- src/main/java/cw_dec/FindOdd.java
+++ src/main/java/cw_dec/FindOdd.java
@@ -15,41 +15,66 @@ Examples
 [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 
+ 0 1 2 3 4 5 6 7 8 9 0 1 2
+[1,1,2,2,2,2,3,3,3,3,3,3,4] should return 4, because it appears 1 time (which is odd).
+
 239 -> 292
  */
 public class FindOdd {
 
   public static int findIt(int[] a) {
+    int result = 0;
+    String numbersString = "";
+    int counter = 1;// count how many times a number appears in array
+    int counterSum = 0;
 
-    return 5;
-  }
+    Arrays.sort(a);
 
-  public static void main(String[] args) {
-    int[] array1 = {0, 1, 0, 1, 0};// 0
-    int[] array2 = {3, 3, 4};// 4
-    int[] array3 = {5, 5, 7, 7, 7};// 0
-    int[] array4 = {1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1};//
-    int[] array5 = {14};//
-//    System.out.println(findIt(array1));//0
-//    System.out.println(findIt(array2));// 4
-//    System.out.println(findIt(array3));// 7
-//    System.out.println(findIt(array4));// 7
-//    System.out.println(findIt(array5));// 7
+    for (int i = 0; i < a.length - 1; i++) {
+      if (a[i] != a[i + 1]) {
+        String nbrText = Integer.toString(a[i]);
+        String counterText = Integer.toString(counter);
 
-    // 2, 3, 5, 7, 11, 13, 17, 19, 23, 29
-//    1 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
-//      2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
-    for (int i = 1; i < 100; i++) {
-      int counter = 0;
-      for (int j = 1; j < i; j++) {
-        if (i % j == 0) {
-          counter++;
-        }
-      }
-      if (counter < 3) {
-        System.out.print(i + " ");
+        /* Represent result in a string pattern "number=>counter"*/
+        numbersString += nbrText.concat("=>").concat(counterText).concat(" ");
+        counterSum += counter;
+        counter = 1;
+      } else {
+        counter++;
       }
     }
 
+    /* Edge case: Find the occurrence(counter) of a last integer */
+    System.out.println(counterSum);
+    int lastNbr = a[a.length - 1];
+    int lastNbrOccur = a.length - counterSum;
+
+    numbersString += Integer.toString(lastNbr).concat("=>").concat(Integer.toString(lastNbrOccur));
+
+    /* • iterate over outer String array
+       • compare counter value agains the odd number
+       • for odd number create an inner loop
+       • once number is found, break of the loop
+       • return an integer representaion of a String number
+     */
+
+
+    System.out.println(numbersString);
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+//    int[] array1 = {0, 1, 0, 1, 0};// 0
+//    int[] array2 = {3, 3, 4};// 4
+    int[] array3 = {5, 5, 7, 7, 7, 8, 8};// 0
+//    int[] array4 = {1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1};//
+//    int[] array5 = {14};//
+//    System.out.println(findIt(array1));//0
+//    System.out.println(findIt(array2));// 4
+    System.out.println(findIt(array3));// 7
+//    System.out.println(findIt(array4));// 7
+//    System.out.println(findIt(array5));// 7
+    System.out.println("miau");
   }
 }
---
+++ src/main/java/onjava8/Main.java
@@ -0,0 +1,7 @@
+package onjava8;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello onjava8");
+  }
+}
